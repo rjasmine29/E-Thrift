fb58b7b484612ae4e2043f77283c906e734603f9
Menelaos <mkaskiris@hotmail.com>
2022-02-04 11:29:40 +0000

    made items model

--- server/items/models.py
+++ server/items/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from ..users.models import Users
 
 # Create your models here.
+class Item(models.Model):
+    
+    item_id = models.PositiveIntegerField()
+    uuid = models.ForeignKey(Users, null=True, on_delete=models.SET_NULL)
+        
+    name = models.CharField(max_lenght=200)
+    price = models.PositiveIntegerField()
+    charity = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    img_url = models.CharField()
+    is_clamed = models.BooleanField()
+    time = models.DateTimeField()
+
+
+
--- server/items/views.py
+++ server/items/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(req):
+    return HttpResponse('<h1>Root items page</h1>')
 
 # Create your views here.
