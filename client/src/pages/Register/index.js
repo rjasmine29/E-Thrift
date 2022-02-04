0b92e7523b40bc187e1a31615b64be5e046c732f
Kelvin Stephano <kstephano@gmail.com>
2022-02-04 22:50:02 +0000

    added remove current img button

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
 import { postRegister, postLogin } from "../../helpers/requests";
 import defaultProfileImg from "../../../assets/default-profile.png";
@@ -20,21 +21,48 @@ const Register = () => {
   const navigate = useNavigate();
   const fileInputRef = useRef();
 
-  const openFiles = e => {
+  /**
+   * Opens the file explorer to select an image.
+   * Triggers the fileInput element using a reference of it with useRef.
+   * 
+   * @param {Click event belonging to element calling the method} e 
+   */
+  const openFiles = (e) => {
     e.preventDefault();
     fileInputRef.current.click();
-  }
+  };
 
-  const onFileSelected = e => {
+  /**
+   * Processes the selected file, asserting that it is a suitable image
+   * and then sets the avatarImg state accordingly.
+   * 
+   * @param {Change event for when an image is selected by the fileInput} e 
+   */
+  const onFileSelected = (e) => {
     const file = e.target.files[0];
     // assert that selected file is an image
     if (file && file.type.substr(0, 5) === "image") {
       setAvatarImg(file);
     } else {
-      setAvatarImg(defaultProfileImg);
+      // sets the avatarImg to null if no image is chosen
+      setAvatarImg(null);
+    }
+  };
+
+  const removeSelectedImage = (e) => {
+    if (avatarImg !== null) {
+      setAvatarImg(null);
+      setPreviewImg(defaultProfileImg);
     }
   }
 
+  /**
+   * Submits the form data to request user registration.
+   * Logs in the user and redirects them to the
+   * home page upon successful registration.
+   *
+   * @param {Submit event that is fired upon submission of the form} e
+   */
   const submitRegister = async (e) => {
     try {
       e.preventDefault();
@@ -47,8 +75,8 @@ const Register = () => {
         password: password,
         passwordConfirm: passwordConfirm,
         phoneNumber: phoneNumber,
-        avatarImg: avatarImg
-      }
+        avatarImg: avatarImg,
+      };
       // register the user
       await postRegister(data);
       // log the user in upon successful register
@@ -56,55 +84,63 @@ const Register = () => {
     } catch (err) {
       console.warn(`Error registering user: ${err}`);
     }
-  }
+  };
 
-    /**
+  /**
    * Requests an authorized log in. Sets local storage user variables
    * upon success and navigates to the home page.
    */
-     async function requestLogin() {
-      try {
-        // obtain access and refresh tokens
-        const { accessToken, refreshToken } = await postLogin({
-          email,
-          password,
-        });
-        const user = jwt_decode(accessToken);
-        localStorage.setItem("accessToken", accessToken);
-        localStorage.setItem("refreshToken", refreshToken);
-        localStorage.setItem("email", user.username);
-        navigate('/');
-      } catch (err) {
-        console.warn(`Error requesting login: ${err}`);
-      }
+  async function requestLogin() {
+    try {
+      // obtain access and refresh tokens
+      const { accessToken, refreshToken } = await postLogin({
+        email,
+        password,
+      });
+      const user = jwt_decode(accessToken);
+      localStorage.setItem("accessToken", accessToken);
+      localStorage.setItem("refreshToken", refreshToken);
+      localStorage.setItem("email", user.username);
+      navigate("/");
+    } catch (err) {
+      console.warn(`Error requesting login: ${err}`);
     }
+  }
 
+  /**
+   * Sets the preview image whenever the selected image (avatarImg)
+   * is changed by the user.
+   */
   useEffect(() => {
-    if (avatarImg) {
+    console.log("Avatar Image Selected:", avatarImg);
+    if (avatarImg && avatarImg !== defaultProfileImg) {
       const reader = new FileReader();
       // set the preview image once avatarImg url has been read
       reader.onloadend = () => {
         setPreviewImg(reader.result);
-      }
+      };
       reader.readAsDataURL(avatarImg);
     } else {
       setPreviewImg(defaultProfileImg);
     }
-  }, [avatarImg])
+  }, [avatarImg]);
 
   return (
     <div className="register-page">
       <form onSubmit={submitRegister}>
-        <img src={previewImg} alt="Profile" onClick={e => openFiles(e)}/>
-        <input
-          type="file"
-          accept="image/*"
-          ref={fileInputRef}
-          className="profile-img"
-          aria-label="profile-input"
-          alt="Input Profile Image"
-          onChange={e => onFileSelected(e)}
-        />
+        <div className="profile-image-container">
+          <CancelOutlinedIcon className="cancel-icon" onClick={removeSelectedImage} />
+          <img className="profile-img" src={previewImg} alt="Profile" onClick={e => openFiles(e)} />
+          <input
+            type="file"
+            accept="image/*"
+            ref={fileInputRef}
+            aria-label="profile-input"
+            hidden={true}
+            onChange={e => onFileSelected(e)}
+          />
+        </div>
+
         <label label="first-name" aria-label="first-name">
           First name
         </label>
@@ -114,6 +150,7 @@ const Register = () => {
           onChange={(e) => setFirstName(e.target.value)}
           required
         />
+
         <label label="last-name" aria-label="last-name">
           Last name
         </label>
@@ -123,6 +160,7 @@ const Register = () => {
           onChange={(e) => setLastName(e.target.value)}
           required
         />
+
         <label label="username" aria-label="username">
           Username
         </label>
@@ -132,6 +170,7 @@ const Register = () => {
           onChange={(e) => setUsername(e.target.value)}
           required
         />
+
         <label label="email" aria-label="email">
           Email address
         </label>
@@ -141,6 +180,7 @@ const Register = () => {
           onChange={(e) => setEmail(e.target.value)}
           required
         />
+
         <label label="password" aria-label="password">
           Password
         </label>
@@ -150,6 +190,7 @@ const Register = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+
         <label label="password-confirm" aria-label="password-confirm">
           Confirm password
         </label>
@@ -159,6 +200,7 @@ const Register = () => {
           onChange={(e) => setPasswordConfirm(e.target.value)}
           required
         />
+
         <label label="phone-number" aria-label="phone-number">
           Phone number
         </label>
@@ -168,7 +210,13 @@ const Register = () => {
           onChange={(e) => setPhoneNumber(e.target.value)}
           required
         />
-        <input type="submit" id="register-btn" className="submit-btn" input="Create your E-Thrift account" />
+
+        <input
+          type="submit"
+          id="register-btn"
+          className="submit-btn"
+          input="Create your E-Thrift account"
+        />
       </form>
     </div>
   );
