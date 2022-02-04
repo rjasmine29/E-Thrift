781d3b80c2604491b29750db29ce6efa0ab04aa2
Jakirul <jakirul.islam@live.co.uk>
2022-02-04 11:51:32 +0000

    added custom model

--- server/server/settings.py
+++ server/server/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users.apps.UsersConfig'
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,8 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ADMIN_LOGIN_REDIRECT_URL = '/admin/'
--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+# ('username', 'first_name', 'last_name', 'email', 'password', 'password_confirmation', 'phone_number', 'avatar_url' )
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField(_('email address'), unique=True)
+    phone_number = models.IntegerField(null=True, blank=True)
+    avatar_url = models.ImageField(null=True, blank=True)
+
+ 
