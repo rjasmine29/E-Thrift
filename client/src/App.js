88a36908e634f4505ef1e4c0edc21cfb15901391
Kelvin Stephano <kstephano@gmail.com>
2022-02-05 10:52:58 +0000

    moved Router location

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import { NavBar } from "./components";
 import { CreateListing, Home, Login, Product, Profile, Register, Search } from "./pages";
@@ -8,20 +8,18 @@ import './pages'
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/create-listing" element={<CreateListing />}></Route>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/product" element={<Product />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/search" element={<Search />}></Route>
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <NavBar />
+      <Routes>
+        <Route path="/create-listing" element={<CreateListing />}></Route>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/product" element={<Product />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+      </Routes>
+    </div>
   );
 }
 
