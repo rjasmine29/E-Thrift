d27aae9c105a30b7e47d7b61cf218368c206dfcb
Kelvin Stephano <kstephano@gmail.com>
2022-02-06 15:03:02 +0000

    removed enzyme

--- client/src/pages/Register/Register.test.js
+++ client/src/pages/Register/Register.test.js
@@ -1,27 +1,34 @@
 import { default as Register } from '.'
-import { screen, render, cleanup } from '@testing-library/react';
-import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
-import { BrowserRouter } from 'react-router-dom';
+import { screen, render, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 describe('Register', () => {
-
-    test('it renders the register form', () => {
-        const registerPage = shallow(
-            <BrowserRouter>
-                <Register />
-            </BrowserRouter>
-        )
-        const tree = toJson(registerPage);
-        expect(tree).toMatchSnapshot();        
+    test('it renders a form', () => {
+        render(<Register />, { wrapper: MemoryRouter });
+        const form = screen.getByRole("form");
+        expect(form).toBeInTheDocument();
     });
 
-    test('it changes preview image when a new image is selected or removed', () => {
+    test('it opens the file explorer when profile image is clicked', () => {
+        render(<Register />, { wrapper: MemoryRouter });
+        const profileImg = screen.getByRole('img', { name: "Profile" });
+        userEvent.click(profileImg);
+        // assert 'openFiles' toHaveBeenCalled() 
+    });
 
+    test('it updates the preview image when the user inputs an image', () => {
+        render(<Register />, { wrapper: MemoryRouter });
+        const profileImg = screen.getByRole('img', { name: "Profile" });
+        const profileInput = screen.getAllByRole('profile-input', { hidden: true }); // ERROR: Cannot find the profile input
+        const testFile = new File(['test'], 'test.png', { type: 'image/png' });
+        const defaultImageSrc = profileImg.src;
+        userEvent.upload(profileInput, testFile);
+        expect(defaultImageSrc).not.toBe(profileImg.src);
     });
 
 
     test('it allows a successful log in with eligible credentials', () => {
-
+        render(<Register />, { wrapper: MemoryRouter });
     });
 });
