5d0c349fd4094aa41a01e3619b17bf0cf216418f
“talhalalala” <talhasadak@outlook.comgit config --global user.name Talhalalala>
2022-02-08 11:11:27 +0000

    committing before a pull

--- client/src/components/productCard/index.js
+++ client/src/components/productCard/index.js
@@ -16,33 +16,35 @@ export default function ProductCard() {
 
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-        
-      <CardMedia
-        component="img"
-        height="140"
-        image="https://static.wikia.nocookie.net/ssb-tourney/images/2/2e/Oogway-white.png"
-        alt="item-image"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Item title
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          item description
-        </Typography>
-        <Typography variant='subtitle.2'>
-            Distance <BsFillGeoAltFill />
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <Button size="small">Share</Button>
-        <Button size="small">More Details</Button>
-      </CardActions>
-    </Card>
+    <div role='product-card'>
+      <Card sx={{ maxWidth: 345 }}>
+          
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://static.wikia.nocookie.net/ssb-tourney/images/2/2e/Oogway-white.png"
+          alt="item-image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Item title
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            item description
+          </Typography>
+          <Typography variant='subtitle.2'>
+              Distance <BsFillGeoAltFill />
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <Button size="small">Share</Button>
+          <Button size="small">More Details</Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
