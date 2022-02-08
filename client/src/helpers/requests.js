2bd7080290a8490b4cc4cd530a8c67a672746b78
Kelvin Stephano <kstephano@gmail.com>
2022-02-08 10:39:35 +0000

    added postEditProfile

--- client/src/helpers/requests.js
+++ client/src/helpers/requests.js
@@ -1,76 +1,106 @@
-const axios = require('axios');
+const axios = require("axios");
 
 async function postLogin(data) {
-    try {
-        const resp = await axios.post(`http://127.0.0.1:8000/user/login`, data);
-        return resp;
-    } catch (err) {
-        console.warn(`Error requesting log in: ${err.response.data.detail}`);
-    }
+  try {
+    const resp = await axios.post(`http://127.0.0.1:8000/user/login`, data);
+    return resp;
+  } catch (err) {
+    console.warn(`Error requesting log in: ${err.response.data.detail}`);
+  }
 }
 
 async function postRegister(data) {
-    try {
-        const resp = await axios.post("http://127.0.0.1:8000/user/register", data);
-        if (resp.data.Error) {
-            return resp.data.Error
-        } else {
-            return resp;
-        }
-    } catch (err) {
-        console.warn(`Error requesting user registration: ${err.response.data.detail}`);
+  try {
+    const resp = await axios.post("http://127.0.0.1:8000/user/register", data);
+    if (resp.data.Error) {
+      return resp.data.Error;
+    } else {
+      return resp;
     }
+  } catch (err) {
+    console.warn(
+      `Error requesting user registration: ${err.response.data.detail}`
+    );
+  }
 }
 
-async function getSearch(data){
-    try {
-        const resp = await axios.get(`http://localhost/search/${data}`);
-        console.log(resp.data);
-        const result = resp.json();
-        return result;
-    } catch (err) {
-        console.warn(`Error retrieving search results for ${data}: ${err}`);
-    }
+async function getSearch(data) {
+  try {
+    const resp = await axios.get(`http://localhost/search/${data}`);
+    console.log(resp.data);
+    const result = resp.json();
+    return result;
+  } catch (err) {
+    console.warn(`Error retrieving search results for ${data}: ${err}`);
+  }
 }
 
 async function getProfile(username) {
-    try {
-        const resp = await axios.get(`http://127.0.0.1:8000/user/${username}`);
-        const user = resp.json();
-        return user;
-    } catch (err) {
-        console.warn(`Error getting profile for ${username}: ${err}`);
-    }
+  try {
+    const resp = await axios.get(`http://127.0.0.1:8000/user/${username}`);
+    const user = resp.json();
+    return user;
+  } catch (err) {
+    console.warn(`Error getting profile for ${username}: ${err}`);
+  }
 }
 
 async function getRating(username) {
-    try {
-        const resp = await axios.get(`http://127.0.0.1:8000/user/rating/${username}`);
-        const rating = resp.json();
-        return rating;
-    } catch (err) {
-        console.warn(`Error getting rating for ${username}: ${err} `)
-    }
+  try {
+    const resp = await axios.get(
+      `http://127.0.0.1:8000/user/rating/${username}`
+    );
+    const rating = resp.json();
+    return rating;
+  } catch (err) {
+    console.warn(`Error getting rating for ${username}: ${err} `);
+  }
 }
 
 async function getActiveItems(username) {
-    try {
-        const resp = await axios.get(`http://127.0.0.1:8000/items/get_by_username/${username}`);
-        const items = resp.json();
-        return items;
-    } catch (err) {
-        console.warn(`Error getting active items for ${username}: ${err}`);
-    }
+  try {
+    const resp = await axios.get(
+      `http://127.0.0.1:8000/items/get_by_username/${username}`
+    );
+    const items = resp.json();
+    return items;
+  } catch (err) {
+    console.warn(`Error getting active items for ${username}: ${err}`);
+  }
 }
 
 async function getClaimedItems(username) {
-    try {
-        const resp = await axios.get(`http://127.0.0.1:8000/items/get_by_username/claimed${username}`);
-        const items = resp.json();
-        return items;
-    } catch (err) {
-        console.warn(`Error getting claimed items for ${username}: ${err}`);
-    }
+  try {
+    const resp = await axios.get(
+      `http://127.0.0.1:8000/items/get_by_username/claimed${username}`
+    );
+    const items = resp.json();
+    return items;
+  } catch (err) {
+    console.warn(`Error getting claimed items for ${username}: ${err}`);
+  }
 }
 
-export { postLogin, postRegister, getSearch, getProfile, getRating, getActiveItems, getClaimedItems }
+async function postEditProfile(data) {
+  try {
+    const resp = await axios.post("http://127.0.0.1:8000/user/edit", data);
+    if (resp.data.Error) {
+      return resp.data.Error;
+    } else {
+      return resp;
+    }
+  } catch (err) {
+    console.warn(`Error editing user ${data.username}: ${err}`);
+  }
+}
+
+export {
+  postLogin,
+  postRegister,
+  getSearch,
+  getProfile,
+  getRating,
+  getActiveItems,
+  getClaimedItems,
+  postEditProfile
+};
