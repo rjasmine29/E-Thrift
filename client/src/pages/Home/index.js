332bbad78128b90d74c729dc0c7e6b7820b10449
Jakirul <jakirul.islam@live.co.uk>
2022-02-08 16:15:37 +0000

    Added carousel

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 // import Carousel from 'react-elastic-carousel'
 import Carousel from "react-multi-carousel";
+import { useNavigate } from 'react-router-dom';
 import "react-multi-carousel/lib/styles.css";
 import styled from 'styled-components'
+import { Image } from "semantic-ui-react";
 import Card from '../../components/Card';
 import "../../App.css"
 import { breakpoints } from '@mui/system';
 
 function Home() {
+    const navigate = useNavigate()
     const breakPoints = [
         { width: 1, itemsToShow: 1 },
         { width: 500, itemsToShow: 2 },
         { width: 700, itemsToShow: 3 },
         { width: 1000, itemsToShow: 4 }
+    ];
+
+    const images = [
+        "https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550330039-a54e15ed9d33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+        "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
     ];
 
     const responsive = {
@@ -37,6 +55,7 @@ function Home() {
         <div className='home-container'>
             <h1>THRIFT & DONATE.</h1>
             <p> Sell your items here...</p>
+            <button onClick={() => navigate('/search')} class="btn btn-success">Find products now</button>
             <img src="../../images/homeImage.png" alt="frontimage" className='homeI' />
             <div className="description">
                 <p> Description: E-THRIFT is a....</p>
@@ -68,12 +87,16 @@ function Home() {
                     dotListClass="custom-dot-list-style"
                     itemClass="carousel-item-padding-40-px"
                 >
-                    <div><img src="../../assets/homeImage.png" /></div>
-                    {/* <div><img src="../../assets/homeImage.png" /></div> */}
-                    <div>Item 2</div>
-                    <div>Item 3</div>
-                    <div>Item 4</div>
-                </Carousel>;
+                    {images.map(image => {
+                        return (
+                            <Image
+                                draggable={false}
+                                style={{ width: "100%", height: "100%" }}
+                                src={image}
+                            />
+                        );
+                    })}
+                </Carousel>
             </div>
         </div>
     )
