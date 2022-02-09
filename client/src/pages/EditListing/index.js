46c60215c3831ef255e8003a31aa63d591cb6881
Jakirul <jakirul.islam@live.co.uk>
2022-02-09 16:17:13 +0000

    Fixed claimed item

--- client/src/pages/EditListing/index.js
+++ client/src/pages/EditListing/index.js
@@ -71,9 +71,6 @@ const EditListing = () => {
 
                 let formData3 = new FormData(e.target);
                 
-                if (e.target.deleteImages.checked) {
-                    console.log("img delete = ", e.target.deleteImages.value)
-                }
                 formData3.append("id",  id)
                 formData3.append("image",  e.target.deleteImages.value)
                 if ( e.target.deleteImages.checked) {
@@ -84,7 +81,6 @@ const EditListing = () => {
                         }
                         let fetching = await fetch("http://127.0.0.1:8000/images/delete/", options1);
                         let jsonfetch = await fetching.json();
-                        console.log("deleting222 =>", jsonfetch)
                     })()
                 }
 
@@ -104,7 +100,6 @@ const EditListing = () => {
 
         const data2 = await fetch(`http://127.0.0.1:8000/images/add/${id}/`, options3)
         const jsondata = await data2.json()
-        console.log("data =>", jsondata)
 
         let name = form.name.value.trim()
         let description = form.description.value.trim()
