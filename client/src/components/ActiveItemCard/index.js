8e0fd57a3387642cb0c650844d7d2c1f0b367f98
Kelvin Stephano <kstephano@gmail.com>
2022-02-09 17:48:40 +0000

    more styling

--- client/src/components/ActiveItemCard/index.js
+++ client/src/components/ActiveItemCard/index.js
@@ -1,42 +1,57 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useNavigate } from 'react-router-dom';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useNavigate } from "react-router-dom";
 
-function ActiveItemCard({ id, name, price, category, description, image_url, image_id }) {
-    const navigate = useNavigate();
+import './style.css';
 
-    const handleCardClick = () => {
-        navigate(`/product-show/:${id}`)
-    }
+function ActiveItemCard({
+  id,
+  name,
+  price,
+  category,
+  description,
+  image_url,
+  image_id,
+}) {
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(`/product-show/:${id}`);
+  };
 
   return (
-    <Card sx={{ maxWidth: 345 }} onClick={handleCardClick} aria-label='active-item-card'>
-      <CardMedia
-        component="img"
-        height="140"
-        image={`https://res.cloudinary.com/deizaqii7/${image_url}`}
-        alt="item-image"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">More Details</Button>
-      </CardActions>
-    </Card>
+    <div className="active-card-wrapper">
+      <Card
+        sx={{ width: '100%' }}
+        onClick={handleCardClick}
+        aria-label="active-item-card"
+      >
+        <CardMedia
+          component="img"
+          height="180"
+          image={`https://res.cloudinary.com/deizaqii7/${image_url}`}
+          alt="item-image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">More Details</Button>
+        </CardActions>
+      </Card>
+    </div>
   );
 }
 
 export default ActiveItemCard;
-
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,8 @@
 export { default as ActiveListings } from './ActiveListings';
 export { default as ActiveItemCard } from './ActiveItemCard';
 export { default as Card } from './Card';
-export { default as CatBar } from './catBar';
+export { default as CatBar } from './CatBar';
+// just change the name of the folder ./catBar to CatBar
 export { default as ClaimedItems } from './ClaimedItems';
 export { default as ClaimedItemCard } from './ClaimedItemCard';
 export { default as EditProfile } from './EditProfile';
