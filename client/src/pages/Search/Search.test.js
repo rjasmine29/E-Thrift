e22d30a936ad37fa0cb1bad9e01c3079a235687b
Kelvin Stephano <kstephano@gmail.com>
2022-02-10 09:35:13 +0000

    reformatted

--- client/src/pages/Search/Search.test.js
+++ client/src/pages/Search/Search.test.js
@@ -1,24 +1,22 @@
 /**
  * @jest-environment jsdom
  */
- import axios from 'axios';
- jest.mock('axios');
- 
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event'
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import {Search} from '../index';
+import { Search } from "../index";
+import axios from "axios";
+jest.mock("axios");
 
 const mockedUsedNavigate = jest.fn();
 
-jest.mock('react-router-dom', () => ({
-   ...jest.requireActual('react-router-dom'),
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockedUsedNavigate,
 }));
 
-describe('search', ()=>{
-
-    test('renders', ()=>{
-        render(<Search />)
-    })
-})
+describe("search", () => {
+  test("renders", () => {
+    render(<Search />);
+  });
+});
